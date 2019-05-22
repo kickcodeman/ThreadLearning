1ad08d89c985e23bb61f0d7a315a12a65c37387d
zempty <kickcodeman@gmail.com>
2019-05-22 17:29:20 +0800

    update learning

--- src/main/java/com/zempty/MainThread.java
+++ src/main/java/com/zempty/MainThread.java
@@ -8,6 +8,19 @@ public class MainThread {
         System.out.println(Thread.currentThread().getName()+"开始运行！");
         Task task = new Task();
         System.out.println("当前的任务对象是 :"+task);
+//        设置线程处于终端状态
+        Thread.currentThread().interrupt();
+//        查看线程的状态
+        System.out.println(Thread.currentThread().isInterrupted());
+
+//        返回线程的中断状态，同时线程状态清零
+        System.out.println(Thread.interrupted());
+        System.out.println(Thread.currentThread().isInterrupted());
+
+//        如果线程没有处于中断状态，返回false
+        System.out.println(Thread.interrupted());
+
+        System.out.println(Thread.currentThread().isInterrupted());
 
         synchronized (task) {
             try {
--- src/main/java/com/zempty/juc/lockSupport/LockSupportTest.java
+++ src/main/java/com/zempty/juc/lockSupport/LockSupportTest.java
@@ -14,6 +14,26 @@ public class LockSupportTest {
         System.out.println();
         System.out.println("主线程执行完毕！");
 
+
+
+//        test for 语句的使用,for语句没有括号，针对一条语句进行循环
+        int j = 5;
+
+        for (int i = 0;  i != j; i++)
+//            if(i==4)
+//                System.out.println(i);
+            System.out.println(i);
+
+//        有时候 if 括号中的语句不仅可以作为判断条件还可以作为代码去执行
+        if (test()) {
+            System.out.println("进入运行");
+        }
+
+    }
+
+    public static boolean test() {
+        System.out.println("test。。。。。。。。。。。");
+        return false;
     }
 
    static class Task implements Runnable {
